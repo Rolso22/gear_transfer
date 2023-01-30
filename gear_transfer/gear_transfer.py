e60c06cb22bfc057165c044737afcad532ef1665
Rolso22 <ramagf@yandex.ru>
2023-01-30 20:08:40 +0300

    fix(class)

--- gear_transfer/gear_transfer.py
+++ gear_transfer/gear_transfer.py
@@ -10,32 +10,67 @@ class Transfer:
         self.buffer_size = buffer_size * 1024  # Kb -> bytes
         self.array = Array(ctypes.c_wchar * self.buffer_size, buffer_count)
 
+    def read_pos_inc(self):
+        self.read_position += 1
+        if self.read_position == self.buffer_count:
+            self.read_position = 0
+
+    def write_pos_inc(self):
+        self.write_position += 1
+        if self.write_position == self.buffer_count:
+            self.write_position = 0
+
     def find(self):
         with self.array.get_lock():
-            while self.array[self.read_position].value == '':
+            pos = self.read_position
+            while self.read_position != pos:
                 if self.read_position == self.buffer_count:
                     self.read_position = 0
-                    return None
-                self.read_position += 1
-            print(f'get in {self.read_position}')
+                if self.array[self.read_position].value != '':
+                    return self.get_elem()
+                else:
+                    self.read_pos_inc()
+            self.read_position = 0
+            return None
+
+    def get_elem(self):
+        if self.array[self.read_position].value != '':
             elem = self.array[self.read_position].value
             self.array[self.read_position].value = ''
-            self.read_position += 1
+            # print(f'get in {self.read_position} -> {self.read_position + 1}')
+            self.read_pos_inc()
             return elem
+        self.read_pos_inc()
+        return None
 
     def get(self):
-        return self.find()
+        elem = self.get_elem()
+        if elem is None:
+            return self.find()
+        else:
+            return elem
+
+    def put_elem(self, data):
+        if self.array[self.write_position].value == '':
+            # print(f'put in {self.write_position}')
+            self.array[self.write_position] = ctypes.create_unicode_buffer(data, self.buffer_size)
+            self.write_pos_inc()
+            return True
+        else:
+            self.write_pos_inc()
+            return False
 
     def put(self, data):
         with self.array.get_lock():
-            while self.array[self.write_position].value != '':
-                if self.write_position == self.buffer_count:
-                    self.write_position = 0
-                    raise "no place"
-                self.write_position += 1
-
-            print(f'put in {self.write_position}')
-            self.array[self.write_position] = ctypes.create_unicode_buffer(data, self.buffer_size)
-            self.write_position += 1
-
+            if not self.put_elem(data):
+                pos = self.write_position
+                while self.write_position != pos:
+                    if self.write_position == self.buffer_count:
+                        self.write_position = 0
+                    if self.array[self.write_position].value == '':
+                        self.put_elem(data)
+                    else:
+                        self.write_pos_inc()
+                self.write_position = 0
+                raise "no place"
 
--- tests/test1.py
+++ tests/test1.py
@@ -1,24 +1,54 @@
+import random
+import time
+
 from gear_transfer.gear_transfer import Transfer
 from multiprocessing import Process
 
+def print_arr(arr):
+    for i in range(len(arr)):
+        print(arr[i].value, end=", ")
 
 def f(tr: Transfer):
-    tr.value.value = 10
-    for i in range(10):
-        tr.put(f'hello from {i}')
-
-    print(f'elem0: {tr.get()}')
-    print(f'elem1: {tr.get()}')
-    print(f'elem2: {tr.get()}')
-    print(f'elem3: {tr.get()}')
+    tr.put({random.randint(0, 10)})
+    tr.put({random.randint(0, 10)})
+    tr.put({random.randint(0, 10)})
+    tr.put({random.randint(0, 10)})
+    tr.put({random.randint(0, 10)})
     print(f'read: {tr.read_position}')
     print(f'write: {tr.write_position}')
+    print_arr(tr.array)
+    print()
 
 
-tr = Transfer(10, 1)
+# def f2(tr: Transfer):
+#     while (True):
+#         tr.get()
+#         tr.get()
+#         print(f'read: {tr.read_position}')
+#         print(f'write: {tr.write_position}')
+#         print_arr(tr.array)
+#         print()
+#         time.sleep(3)
+
+
+tr = Transfer(5, 1)
 
 p = Process(target=f, args=(tr,))
 p.start()
 p.join()
+
+for i in range(5):
+    print(tr.array[i].value)
+
+for i in range(7):
+    print(f'{i} : {tr.get()}')
+
+print(tr.read_position)
+print_arr(tr.array)
+
+time.sleep(2)
+# p2 = Process(target=f2, args=(tr,))
+# p2.start()
+# p2.join()
 
 
